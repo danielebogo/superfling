35e37f5ceaafe59ab52f46061c1975e89b147f23
Daniele Bogo <bogodaniele83@gmail.com>
2016-05-12 21:53:20 +0100

    COnfigure project

---
+++ Pods/Target Support Files/Pods-superfling/Pods-superfling-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_superfling : NSObject
+@end
+@implementation PodsDummy_Pods_superfling
+@end
---
+++ superfling/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  superfling
+//
+//  Created by Daniele Bogo on 12/05/2016.
+//  Copyright © 2016 Daniele Bogo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
